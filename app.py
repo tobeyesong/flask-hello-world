04dacace9c3b227c87370476740162808eb07d36
Toan Lam <toanlam01@gmail.com>
2024-11-06 19:08:20 -0800

    add db_create

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ def db_test():
     conn = psycopg2.connect("postgresql://lab10_db_97vf_user:F2SHYRLNi59nqfsfJ7PpMp0aXmyMzsjn@dpg-csm2r1bqf0us73frn24g-a/lab10_db_97vf")
     conn.close()
     return "Database connection successful!"
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgresql://lab10_db_97vf_user:F2SHYRLNi59nqfsfJ7PpMp0aXmyMzsjn@dpg-csm2r1bqf0us73frn24g-a/lab10_db_97vf")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+        );
+    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Basketball Table Created"
