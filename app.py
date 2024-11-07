d631b8212d91c05f3832b80ce1d2b712acffbcfa
Toan Lam <toanlam01@gmail.com>
2024-11-06 19:18:11 -0800

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World from --your name-- in 3308"
+    return "Hello World from --Toan Lam-- in 3308"
 
 @app.route('/db_test')
 def db_test():
