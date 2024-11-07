bee4c7a267ef2a3147979b035d12450ce227b122
Toan Lam <toanlam01@gmail.com>
2024-11-06 19:03:38 -0800

    add db

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +6,8 @@ app = Flask(__name__)
 def index():
     return "Hello World from --your name-- in 3308"
 
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://lab10_db_97vf_user:F2SHYRLNi59nqfsfJ7PpMp0aXmyMzsjn@dpg-csm2r1bqf0us73frn24g-a/lab10_db_97vf")
+    conn.close()
+    return "Database connection successful!"
